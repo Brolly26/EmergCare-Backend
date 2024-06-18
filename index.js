0138d964d1c3e406baa4f03606674b861f16af31
GabrielCode <bielaugusto8@hotmail.com>
2024-06-18 01:51:02 -0300

    ajuste

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import authRoute from './Routes/auth.js';
 import userRoute from './Routes/user.js';
 import doctorRouter from './Routes/doctor.js';
@@ -14,13 +14,33 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Configuração de CORS
 const corsOptions = {
-  origin: process.env.CLIENT_SITE_URL, // Altere aqui para permitir solicitações do seu frontend no Vercel
-  credentials: true,
+  origin: '*', // Permite todas as origens durante o desenvolvimento
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos permitidos
+  allowedHeaders: ['Content-Type', 'Authorization'], // Headers permitidos
+  credentials: true, // Permite cookies e outros credenciais
 };
 
+app.use(cors(corsOptions)); // Aplica o middleware de CORS
+
+// Middleware para lidar com requisições preflight OPTIONS
+app.options('*', cors(corsOptions));
+
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+
+// Rotas
+app.use('/api/v1/auth', authRoute);
+app.use('/api/v1/users', userRoute);
+app.use('/api/v1/doctors', doctorRouter);
+app.use('/api/v1/reviews', reviewRoute);
+app.use('/api/v1/bookings', bookingRoute);
+
+// Rota de teste para verificar se o servidor está funcionando
 app.get('/', (req, res) => {
-  res.send('Api está funcionando');
+  res.send('API está funcionando');
 });
 
 // Conexão com o banco de dados
@@ -28,25 +48,13 @@ mongoose.set('strictQuery', false);
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL, {});
-    console.log("Conectado ao banco de dados com sucesso");
+    console.log('Conectado ao banco de dados com sucesso');
   } catch (error) {
     console.error('Não foi possível conectar ao banco de dados:', error.message);
   }
 };
 
-// Middleware
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors(corsOptions));
-
-// Rotas
-app.use('/api/v1/auth', authRoute); // domain/api/v1/auth/register
-app.use('/api/v1/users', userRoute);
-app.use('/api/v1/doctors', doctorRouter);
-app.use('/api/v1/reviews', reviewRoute);
-app.use('/api/v1/bookings', bookingRoute);
-
 app.listen(port, () => {
   connectDB();
-  console.log("Servidor está rodando na porta " + port);
+  console.log('Servidor está rodando na porta ' + port);
 });
