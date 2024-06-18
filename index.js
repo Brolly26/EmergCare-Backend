18c05271737e32eac68eb2fe14a1458163ff1693
GabrielCode <bielaugusto8@hotmail.com>
2024-06-17 21:54:48 -0300

    Ligacao Banco

--- index.js
+++ index.js
@@ -1,55 +1,52 @@
-import express from 'express'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import authRoute from './Routes/auth.js'
-import userRoute from './Routes/user.js'
-import doctorRouter from './Routes/doctor.js'
-import reviewRoute from './Routes/review.js'
-import bookingRoute from './Routes/booking.js'
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import authRoute from './Routes/auth.js';
+import userRoute from './Routes/user.js';
+import doctorRouter from './Routes/doctor.js';
+import reviewRoute from './Routes/review.js';
+import bookingRoute from './Routes/booking.js';
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const port = process.env.PORT || 8000
-
+const app = express();
+const port = process.env.PORT || 5000;
 
 const corsOptions = {
-    origin:true
-}
+  origin: process.env.CLIENT_SITE_URL, // Altere aqui para permitir solicitações do seu frontend no Vercel
+  credentials: true,
+};
 
-app.get('/', (req,res)=>{
-    res.send('Api está funcionando')
-})
-//database connection
-mongoose.set('strictQuery', false)
-const connectDB = async() =>{
-    try {
-        await mongoose.connect(process.env.MONGO_URL, {});
-        console.log("CONNECTED TO DATABASE SUCCESSFULLY");
+app.get('/', (req, res) => {
+  res.send('Api está funcionando');
+});
 
-    } catch (error) {
-        console.error('COULD NOT CONNECT TO DATABASE:', error.message);
-    }
+// Conexão com o banco de dados
+mongoose.set('strictQuery', false);
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {});
+    console.log("Conectado ao banco de dados com sucesso");
+  } catch (error) {
+    console.error('Não foi possível conectar ao banco de dados:', error.message);
+  }
+};
 
-    } 
-
-
-
-// middleware
+// Middleware
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
-app.use('/api/v1/auth', authRoute) // domain/api/v1/auth/register
-app.use('/api/v1/users', userRoute) 
-app.use('/api/v1/doctors', doctorRouter) 
-app.use('/api/v1/reviews', reviewRoute) 
-app.use('/api/v1/bookings', bookingRoute) 
 
+// Rotas
+app.use('/api/v1/auth', authRoute); // domain/api/v1/auth/register
+app.use('/api/v1/users', userRoute);
+app.use('/api/v1/doctors', doctorRouter);
+app.use('/api/v1/reviews', reviewRoute);
+app.use('/api/v1/bookings', bookingRoute);
 
-app.listen(port, ()=>{
-    connectDB();
-    console.log("Server está rodando na porta " + port)
-})
-// Gabriel2213  emergcarebackend
+app.listen(port, () => {
+  connectDB();
+  console.log("Servidor está rodando na porta " + port);
+});
